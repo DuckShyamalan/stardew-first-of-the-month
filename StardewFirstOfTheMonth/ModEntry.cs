70dd06146645d065efdcbda90f951e59ed4e0877
Syed Fatir <syedfatir@hotmail.com>
2023-12-06 23:46:28 +0400

    initialisation

--- StardewFirstOfTheMonth/ModEntry.cs
+++ StardewFirstOfTheMonth/ModEntry.cs
@@ -7,9 +7,15 @@ using StardewValley;
 
 namespace StardewFirstOfTheMonth {
 
-    public class ModEntry : Mod
-    {
-
+    public class ModEntry : Mod {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper) {
+            //helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
     }
 
 }
