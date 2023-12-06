fdbca8353f1d853b094fd3e752990426fcdb2eb6
Syed Fatir <syedfatir@hotmail.com>
2023-12-06 23:46:16 +0400

    initialisation

---
+++ StardewFirstOfTheMonth/ModEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace StardewFirstOfTheMonth {
+
+    public class ModEntry : Mod
+    {
+
+    }
+
+}
