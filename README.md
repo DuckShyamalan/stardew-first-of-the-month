3b8c10c5bda00f7d237c014edde0171a6bc42983
Syed Fatir <syedfatir@hotmail.com>
2024-01-08 19:23:34 +0400

    added sound

--- README.md
+++ README.md
@@ -4,5 +4,14 @@ Greets you with a dialogue box when you wake up on the first of the month in Sta
 ## Usage
 ### From Source Code
 1. run `dotnet build` in the directory of the code.
-2. If not already in Stardew Valley's Mods folder, move the result of Step 1 into it.
-3. Run the game through SMAPI.
+2. Copy the attached `.wav` file to the folder where this mod is located and then run the game.
+3. If not already in Stardew Valley's Mods folder, move the result of Step 1 into it.
+4. Run the game through SMAPI.
+
+#### Dependencies:
+- SMAPI
+
+https://stardewvalleywiki.com/Modding:Index
+
+#### Assets:
+https://tuna.voicemod.net/sound/fe5eb2de-854b-4ccf-b5c6-bc4df4e08e87
--- StardewFirstOfTheMonth/ModEntry.cs
+++ StardewFirstOfTheMonth/ModEntry.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -15,6 +17,27 @@ namespace StardewFirstOfTheMonth {
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper) {
+            CueDefinition myCueDefinition = new CueDefinition();
+
+            // Adding the name for the cue, which will be
+            // the name of the audio to play when using sound functions.
+            myCueDefinition.name = "wakeUp";
+            myCueDefinition.instanceLimit = 1;
+            myCueDefinition.limitBehavior = CueDefinition.LimitBehavior.ReplaceOldest;
+
+            // Get the audio file and add it to a SoundEffect.
+            SoundEffect audio;
+            string filePathCombined = Path.Combine(this.Helper.DirectoryPath, "voicemod-wake-up-highpitch.wav");
+            using (var stream = new System.IO.FileStream(filePathCombined, System.IO.FileMode.Open)) {
+                audio = SoundEffect.FromStream(stream);
+            }
+
+            // Setting the sound effect to the new cue.
+            myCueDefinition.SetSound(audio, Game1.audioEngine.GetCategoryIndex("Sound"), false);
+
+            // Adding the cue to the sound bank.
+            Game1.soundBank.AddCue(myCueDefinition);
+
             helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
         }
 
@@ -31,6 +54,8 @@ namespace StardewFirstOfTheMonth {
             if (Game1.dayOfMonth == 1 && Game1.timeOfDay == 600) {
                 string message = "WAKE UP! IT'S THE FIRST OF THE MONTH";
                 Game1.activeClickableMenu = new DialogueBox(message);
+                Game1.playSound("wakeUp");
+
                 //this.Monitor.Log(message, LogLevel.Debug);
             }
         }
